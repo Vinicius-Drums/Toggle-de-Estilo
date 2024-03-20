1e97ea4274741c89f1bfc0d95a03cc8e626e6f93
Vincius-Drums <anaogeraldino@gmail.com>
2024-03-20 00:24:35 -0300

    Commit para adiçao de comentarios

--- src/components/StyleToggle.tsx
+++ src/components/StyleToggle.tsx
@@ -1,37 +1,31 @@
-// StyleToggle.tsx
-
 import React, { useEffect, useState } from "react";
-import "./StyleToggle.css"; // Importa o arquivo de estilos associado ao componente
+import "./StyleToggle.tsx";
 
-// Definição do componente funcional StyleToggle
 const StyleToggle: React.FC = () => {
-  const [style, setStyle] = useState<"light" | "dark">("light"); // Define o estado 'style' para controlar o tema da aplicação
+  const [style, setStyle] = useState<"light" | "dark">("light");
 
-  // Efeito para atualizar as classes no corpo do documento baseado no estilo selecionado
   useEffect(() => {
-    document.body.classList.remove("light", "dark"); // Remove classes de tema anteriores
-    document.body.classList.add(style); // Adiciona a classe do tema selecionado
+    document.body.classList.remove("light", "dark");
+    document.body.classList.add(style);
   }, [style]);
 
-  // Função para alternar entre os estilos
   const toggleStyle = () => {
-    setStyle((prevStyle) => (prevStyle === "light" ? "dark" : "light")); // Alterna entre os temas light e dark
+    setStyle((prevStyle) => (prevStyle === "light" ? "dark" : "light"));
   };
 
-  // Renderização do componente
   return (
     <div className="StyleToggle">
       <div className="banda-content">
-        <h1>Banda Leão Livre</h1> {/* Título do aplicativo */}
+        <h1>Banda Leão Livre</h1>
         <img
           src="src/Imagens/BandaLeaolivre.jpeg"
           alt="Banda Leão Livre"
           className="banda-imagem"
-        /> {/* Imagem da banda */}
+        />
       </div>
-      <button onClick={toggleStyle}>Toggle Style &#127761;</button> {/* Botão para alternar entre os estilos */}
+      <button onClick={toggleStyle}>Toggle Style &#127761;</button>
     </div>
   );
 };
 
-export default StyleToggle; // Exporta o componente StyleToggle para ser utilizado em outros arquivos
+export default StyleToggle;
